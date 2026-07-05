e10314d0f9a384cd7a5c8a7a2647f2a4d1771a58
Russell McQueeney <rmcq@utexas.edu>
2026-07-04 23:21:07 -0700

    moneydisplay

---
+++ lever_simple_marker.gd
@@ -0,0 +1,3 @@
+extends Node2D
+func _draw():
+	draw_circle(Vector2.ZERO, 10, Color.YELLOW)
---
+++ money_display.gd
@@ -0,0 +1,20 @@
+extends Marker2D
+
+@export var digit_spacing := 24.0
+
+var amount := 0
+
+func _draw():
+	var font := ThemeDB.fallback_font
+	var size := ThemeDB.fallback_font_size
+	var onespos := Vector2(-5, 0.5*size)
+	var s = "%04d" % [amount]
+
+	for i in range(len(s)):
+		draw_string(font, onespos + Vector2(-digit_spacing * i, 0), s[3 - i], HORIZONTAL_ALIGNMENT_CENTER, -1, size, Color.BLACK)
+
+func add(x: int):
+	amount += x
+	queue_redraw()
+
+	
