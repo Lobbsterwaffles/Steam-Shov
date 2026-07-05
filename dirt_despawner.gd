59d74677c119462dec7722346560bf5be13e1f61
Russell McQueeney <rmcq@utexas.edu>
2026-07-04 23:42:20 -0700

    moneydisplay2

--- dirt_despawner.gd
+++ dirt_despawner.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var money_per_dirt := 1
 
 var despawning := 1.0
 var despawn_max := 1.0
@@ -23,6 +24,7 @@ func _process(delta: float) -> void:
 	while despawning > 0 and not pending_despawn.is_empty():
 		var to_destroy = pending_despawn.pop_back()
 		if is_instance_valid(to_destroy) == true:			
+			%money.add(money_per_dirt)
 			to_destroy.queue_free()
 			despawning -= 1
 		
