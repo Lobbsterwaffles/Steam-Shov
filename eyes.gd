f4bd8842abef3edc26e7b54bd30ae2ff2bb84b70
Russell McQueeney <rmcq@utexas.edu>
2026-07-05 09:47:12 -0700

    cateye

--- eyes.gd
+++ eyes.gd
@@ -4,20 +4,28 @@ extends Node2D
 @export var eye_pupil := Color.BLACK
 @export var eye_liner := Color.BLACK
 
+@export var ellipse_major_radius := 10.0
+@export var ellipse_minor_radius := 5.0
+@export var pupil_major_radius := 3.0
+@export var pupil_minor_radius := 3.0
+@export var pupil_edge_offset_frac := 0.5 
+
 func _process(dt):
 	queue_redraw()
 
 func _draw():
-	const major := 10.0
-	const minor := 5.0
+	var major = ellipse_major_radius
+	var minor = ellipse_minor_radius
 
 	draw_ellipse(Vector2.ZERO, major, minor, eye_white)
 	draw_ellipse(Vector2.ZERO, major, minor, eye_liner, false, 2.0, true)
 	var target = to_local(%scoop.global_position).normalized()
 	var epnt = ellipse_ray(target,  major, minor)
-	const rdot = 3
-	var epoff = ellipse_offset(epnt, major, minor, -0.5*rdot)
-	draw_circle(epoff, rdot, eye_pupil)
+	var pr = pupil_major_radius 
+	var epoff = ellipse_offset(epnt, major, minor, -1 * pr * pupil_edge_offset_frac)
+	draw_ellipse(epoff, pupil_major_radius, pupil_minor_radius, eye_pupil, true, -1, true)
+
+	# draw_circle(epoff, pupil_radius, eye_pupil, true, -1, true)
 
 func ellipse_ray(v: Vector2, a: float, b: float) -> Vector2:
 	if v == Vector2.ZERO:
